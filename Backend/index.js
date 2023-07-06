8c9dadb6702b9305a6040984b616df5b0ea6518a
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-07-07 01:48:01 +0530

    done

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,21 @@ const { dataBaseConnection } = require("./Configs/dataBase.js");
 require("dotenv").config();
 
 const cors = require("cors");
+const { userRouter } = require("./Routes/userRouter.js");
+const { oemSpecsRouter } = require("./Routes/oemSpecsRouter.js");
+const { invetoryRouter } = require("./Routes/marketPlaceInvetoryRouter.js");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/", userRouter);
+
+app.use("/", oemSpecsRouter);
+
+app.use("/", invetoryRouter);
 
 app.listen(process.env.PORT, async () => {
   try {
